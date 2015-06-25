c771b09d746e92b5329307a8cc2bf896180d94e6
jonathansayer <jon.sayer1@gmail.com>
2015-06-25 14:24:30 +0100

    computer ships placed

--- lib/battlehips_web.rb
+++ lib/battlehips_web.rb
@@ -34,12 +34,18 @@ class BattleshipsWeb < Sinatra::Base
     $game.player_1.place_ship Ship.cruiser, params[:coordinates3], params[:orientation3]
     $game.player_1.place_ship Ship.battleship, params[:coordinates4], params[:orientation4]
     $game.player_1.place_ship Ship.aircraft_carrier, params[:coordinates5], params[:orientation5]
+    $game.player_2.place_ship Ship.submarine, :A3, :vertically
+    $game.player_2.place_ship Ship.destroyer, :H10, :horizontally
+    $game.player_2.place_ship Ship.cruiser, :J2, :vertically
+    $game.player_2.place_ship Ship.battleship, :D1, :vertically
+    $game.player_2.place_ship Ship.aircraft_carrier, :A10, :horizontally
     erb :placed_ships
   end
 
   get '/action' do
     @aim = params[:aim]
     $game.player_1.shoot @aim.to_sym
+    $game.player_1.shoot @aim.
     erb :action
   end
 end
